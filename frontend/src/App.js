40cec376fc3702d6145c09703f6f74d4e6acbde3
Mario Villatoro <45154587+444mars@users.noreply.github.com>
2022-11-17 13:16:00 -0600

    switched around index.js and app.js to app.js to start adding public and private dash layouts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,28 @@
-import { Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
 import Homepage from './pages/Homepage';
 import AboutPage from './pages/AboutPage';
 import FileClaimsPage from './pages/FileClaimsPage';
 import ClosedClaimsPage from './pages/ClosedClaimsPage';
+import LoginPage from './pages/LoginPage';
+import Layout from './components/Layout';
+
 
 
 function App() {
   return (
-    <Routes>
-      <Route path='/*' element={<Homepage />}>
-      </Route> 
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/*' element={<Homepage />} />
+          <Route index element={<Homepage />} />
+          <Route path="Homepage" element={<Homepage />} />
+          <Route path="AboutPage" element={<AboutPage />} />
+          <Route path="FileClaimsPage" element={<FileClaimsPage />} />
+          <Route path="ClosedClaimsPage" element={<ClosedClaimsPage />} />
+          <Route path="LoginPage" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
